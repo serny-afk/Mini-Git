f842eac5691166aa9358dbf9e01838b97a701c66
serny-afk <sernyuanong@gmail.com>
2025-09-06 19:42:58 +0800

    repo skeleton implemented

--- src/minigit/Blob.java
+++ src/minigit/Blob.java
@@ -5,15 +5,18 @@ import interfaces.IIndex;
 import interfaces.IRepository;
 
 public class Blob implements IBlob {
-    private String filename;
+    private String content;
     private String hash;
 
     // tracks and stores file content (string / byte array)
     // SHA-1 hash
 
-    public Blob(String filename, String hash) {
-        this.filename = filename;
-        this.hash = hash;
+    public Blob(String content) {
+        this.content = content;
+        // this.hash = Utils.hashContent(content);
+
+        // ** Note: Utils.hashContent(String content) is a method to be implemented later on
+        // which will be under utils.java, to compute SHA-1 hash **
     }
 
     @Override
--- src/minigit/Repository.java
+++ src/minigit/Repository.java
@@ -1,4 +1,11 @@
 package minigit;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.*;
+
 import interfaces.IBlob;
 import interfaces.IIndex;
 import interfaces.IRepository;
@@ -6,35 +13,74 @@ import interfaces.IRepository;
 public class Repository implements IRepository {
 
     // TODO: manage commits, blobs, index
+    private final IIndex index; // staging area
+    private final String repoPath = ".minigit"; // root folder
+    private final List<Map<String, String>> commits = new ArrayList<>(); // commits
 
-    public Repository() {
-        // constructor
+
+    public Repository(IIndex index) {
+        this.index = index;
     }
 
     @Override
     public void init() {
-        System.out.println("Repo Initialized");
-        // i.e. git init method
+        // creates a ".minigit/blobs" folder
+        try {
+            Path blobsDir = Paths.get(repoPath, "blobs");
+            Files.createDirectories(blobsDir);
+            System.out.println("Initialized empty minigit repo in " + this.repoPath);
+        } catch (IOException e) {
+            e.printStackTrace(); // replace with throw new exception later on
+        }
     }
 
     @Override
     public void add(String filename) {
+        try {
+            //read file content
+            String content = Files.readString(Paths.get(filename));
 
+            // blob creation and saving
+            IBlob blob = new Blob(content);
+            blob.saveBlob();
+
+            // stage in index
+            this.index.add(filename, blob.getHash());
+
+        } catch (IOException e) {
+            e.printStackTrace(); // replace with throw new exception later on
+        }
     }
 
     @Override
     public void commit(String message) {
+        Map<String, String> snapshot = new HashMap<>();
 
+        // essentially takes staged files and stores them with a message
+        for (String file : this.index.listFiles()) {
+            snapshot.put(file, this.index.getBlobHash(file));
+        }
+        this.commits.add(snapshot);
+
+        // simulates commit -m "message"
+        System.out.println("Committed: " + message);
     }
 
     @Override
     public void log() {
-
+        // prints out commits in reverse order
+        for (int i = this.commits.size() - 1; i >= 0; i--) {
+            System.out.println("Commit #" + (i + 1));
+            this.commits.get(i).forEach((file, hash) -> System.out.println(file + " -> " + hash));
+            System.out.println();
+        }
     }
 
     @Override
     public void status() {
-
+        System.out.println("Staged files:");
+        for (String file : this.index.listFiles()) {
+            System.out.println(file + " -> " + this.index.getBlobHash(file));
+        }
     }
-
 }
