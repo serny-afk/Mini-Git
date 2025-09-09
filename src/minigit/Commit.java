2125ae60f88bf116430da4e6779fa1c682f0478e
raywangruihua <154847819+raywangruihua@users.noreply.github.com>
2025-09-09 21:30:51 +0800

    Did some work on commit, added new method to Utils to create prefix before hashing to create oid, have not changed the sha1 method to use this change

--- src/minigit/Commit.java
+++ src/minigit/Commit.java
@@ -6,8 +6,8 @@ import java.util.*;
  * Metadata contains
  * tree_oid
  * parent (commits)
- * author
- * commiter
+ * author (optional)
+ * commiter (optional)
  * commit message
  * 
  * How to make a commit
@@ -22,21 +22,62 @@ import java.util.*;
  * If another tree_oid, go down that tree
  * If blob oid, recreate blob in working directory to "load" state
  * Repeat until end of tree reach, oid = None
+ * 
+ * 
  */
 
 public class Commit {
-    int treeOid;
-    String message;
-    Blob blob;
+    private String treeOid;
+    private String parents[];
+    private String message;
+    public String objectDirectory = "./minigit/objects";
 
     public Commit() {}
 
-    public Commit(String message, String content) {
+    public Commit(String treeOid, String message) {
+        this.treeOid = treeOid;
         this.message = message;
-        blob
     }
 
-    public int createCommit() {
+    // constructors
+    public void setTreeOid(String treeOid) {
+        this.treeOid = treeOid;
+    }
 
+    public void setParents(String parents[]) {
+        this.parents = parents;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // accessors
+    public String getTreeOid() {
+        return treeOid;
+    }
+
+    public String[] getParents() {
+        return parents;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    // unique methods
+    public String parseData() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(treeOid);
+        for (String parent : parents) {
+            sb.append(parent);
+        }
+        sb.append(message);
+        return sb.toString();
+    }
+
+    public void commit() {
+        String data = parseData();
+        String hash = Utils.sha1(Utils.prefix("commit", data));
     }
 }
--- src/minigit/Utils.java
+++ src/minigit/Utils.java
@@ -3,6 +3,24 @@ package minigit;
 import java.security.*;
 
 public class Utils {
+    // create byte sequence to hash in sha1
+    // data is a string of all metadata and file content of the object
+    public static byte[] prefix(String type, String data) throws Exception{
+        byte[] dataBytes = data.getBytes("UTF-8");
+
+        int size = dataBytes.length;
+        String header = type + " " + size;
+        byte[] headerBytes = header.getBytes("UTF-8");
+
+        byte[] sequence = new byte[headerBytes.length + 1 + dataBytes.length];
+
+        sequence[headerBytes.length] = 0;
+        System.arraycopy(headerBytes, 0, sequence, headerBytes.length+1, dataBytes.length);
+        return sequence;
+    }
+
+    // takes in byte sequence created in prefix to create unique object id
+    // which acts as filepath for object in minigit/object/{hash}
     public static String sha1(String input) {
         // SHA-1 hash (40 char hexadecimal string) gives
         // each object a unique identifier like in git
